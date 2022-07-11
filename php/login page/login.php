ed5f485930a87836562883d10dff2f4965d435a9
MahanHomayouniAria <mahanhomayouniaria@gmail.com>
2022-07-11 14:03:01 +0430

    New

--- php/login page/login.php
+++ php/login page/login.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_GET["username"]) && isset($_GET["passworwd"]) in_array($_GET["username"]) && in_array($_GET["passworwd"]) ){
+if(isset($_GET["username"]) && isset($_GET["passworwd"]) && in_array($_GET["username"]) && in_array($_GET["passworwd"]) ){
     $username = $_GET["username"];
     $password = $_GET["password"];
 }else{
