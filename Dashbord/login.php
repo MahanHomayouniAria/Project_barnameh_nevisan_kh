dbbacae93496bc10913d530a1b2e4de1b4d0a46a
MahanHomayouniAria <mahanhomayouniaria@gmail.com>
2022-07-12 21:05:51 +0430

    .

--- Dashbord/login.php
+++ Dashbord/login.php
@@ -3,7 +3,7 @@
 $userename = $_GET["username"];
 $password = $_GET["password"];
 $arr = array("Javad" , "Mahan" , "Amir");
-$x = header("Location: http://www.yoursite.com/new_index.php", TRUE, 301); exit();
+// $x = header("Location: http://www.yoursite.com/new_index.php", TRUE, 301); exit();
     
 if (isset($_SESSION["usename"])) {
     header("location: Dashbord.php");
