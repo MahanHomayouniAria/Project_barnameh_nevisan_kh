1df7fbd8d592920ad1b4c27c2069256d4cd1d390
MahanHomayouniAria <mahanhomayouniaria@gmail.com>
2022-07-24 12:24:09 +0430

    پروژه جدید

--- my blog project/savepost.php
+++ my blog project/savepost.php
@@ -1,21 +1,48 @@
 <?php
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "myblog";
+
+
+// // Create connection
+// $conn = mysqli_connect($servername, $username, $password,$dbname);
+
+// // Check connection
+// if (!$conn) {
+//   die("Connection failed: " . mysqli_connect_error());
+// }
+// echo "Connected successfully";
+
+
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "myblog";
 
-
 // Create connection
-$conn = mysqli_connect($servername, $username, $password,$dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
 
-$title = $_get ["title"];
-$content = $_get ["content"];
+
+$title =$_get["title"];
+$content =$_get["content"];
+
 $sql = "INSERT INTO posts (title,content) Values ('".$title."','".$content."')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
 
 ?>
